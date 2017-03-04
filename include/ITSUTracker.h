8f5a981338d8edfe976b6490f360724b527f0c8c
GlassOfWhiskey <iacopo.c92@gmail.com>
2017-03-04 16:25:10 +0100

    Aggiunta gestione doxygen Creazione classe ITSUTracker

---
+++ include/ITSUTracker.h
@@ -0,0 +1,16 @@
+/// \file ITSUTracker.h
+/// \brief 
+///
+/// \author Iacopo Colonnelli, Politecnico di Torino
+
+#ifndef SRC_ITSUTRACKER_H_
+#define SRC_ITSUTRACKER_H_
+
+class ITSUTracker
+{
+  public:
+    ITSUTracker();
+    virtual ~ITSUTracker();
+};
+
+#endif /* SRC_ITSUTRACKER_H_ */
---
+++ src/ITSUTracker.cpp
@@ -0,0 +1,18 @@
+/// \file ITSUTracker.cpp
+/// \brief 
+///
+/// \author Iacopo Colonnelli, Politecnico di Torino
+
+#include "ITSUTracker.h"
+
+ITSUTracker::ITSUTracker()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+ITSUTracker::~ITSUTracker()
+{
+  // TODO Auto-generated destructor stub
+}
+
