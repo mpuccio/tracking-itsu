686518499c224f29e93c5eb3202bae0a4626f84d
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2017-09-26 01:05:39 +0200

    Fix copyright notice following O2 prescriptions

--- src/CA/Road.cxx
+++ src/CA/Road.cxx
@@ -1,20 +1,16 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
 /// \file Road.cxx
 /// \brief 
 ///
-/// \author Iacopo Colonnelli, Politecnico di Torino
-///
-/// \copyright Copyright (C) 2017  Iacopo Colonnelli. \n\n
-///   This program is free software: you can redistribute it and/or modify
-///   it under the terms of the GNU General Public License as published by
-///   the Free Software Foundation, either version 3 of the License, or
-///   (at your option) any later version. \n\n
-///   This program is distributed in the hope that it will be useful,
-///   but WITHOUT ANY WARRANTY; without even the implied warranty of
-///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-///   GNU General Public License for more details. \n\n
-///   You should have received a copy of the GNU General Public License
-///   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-///////////////////////////////////////////////////////////////////////////////
 
 #include "ITSReconstruction/CA/Road.h"
 
